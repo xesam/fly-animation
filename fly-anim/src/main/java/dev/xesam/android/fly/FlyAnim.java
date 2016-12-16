999d3431a1f7b5383336a31d87325069bb99b040
xesam <xesamguo@gmail.com>
2016-12-16 14:11:01 +0800

    FlyAnim

---
+++ fly-anim/src/main/java/dev/xesam/android/fly/FlyAnim.java
@@ -0,0 +1,26 @@
+package dev.xesam.android.fly;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by xe on 16-12-16.
+ */
+
+public class FlyAnim {
+    private ViewGroup vParent;
+    private View vFrom;
+    private View vTo;
+
+    public void from(View from) {
+
+    }
+
+    public void to(View to) {
+
+    }
+
+    public void start() {
+
+    }
+}
